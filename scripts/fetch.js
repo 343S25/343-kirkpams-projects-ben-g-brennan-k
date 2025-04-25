c9a1540ed983b8f6a5060fc97b6e5c96b88ab216
Benjamin Glass <glassbg@dukes.jmu.edu>
2025-04-25 14:10:55 -0400

    template

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,19 +5,22 @@ async function get_all_products() {
   return json;
 }
 
-// Filter the json array into useful information 
-function filter_json(){
-
-}
+// Filter the json array into useful information
+function filter_json() {}
 
 function update_grid(products) {
-// TODO get the grid and then put the items in
-// TODO create html <template>
+  // Get the grid and then put the items in
+  let list = document.getElementById('grid-boxes');
+  let template = document.getElementById('template');
+  for (let i = 0; i < 18; i++) {
+    let grid_item = template.content.cloneNode(true);
+    grid_item.product_link.textcontent = "Test";
+    list.appendChild(grid_item);
+  }
 }
 
 
 (async function() {
   let products = await get_all_products();
-  
-  
+  update_grid();
 })();

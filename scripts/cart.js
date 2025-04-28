fdf4b56923a45eb885938d3f780a61320a55ce22
Benjamin Glass <glassbg@dukes.jmu.edu>
2025-04-28 14:22:26 -0400

    Inital localstorage implementation

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,5 +1,28 @@
-function get_Cart_Items(){
-    return localStorage.getItem("cart");
+function get_Cart_Items() {
+  let items = localStorage.getItem('cart');
+  if (items != undefined) {
+    return JSON.parse(items);
+  }
+  return [];
 }
 
-// add_Cart_Item
+function add_Cart_Item(item) {
+  let items = get_Cart_Items();
+  let unique_item = items.find((i) => i.id);
+  if (unique_item != undefined) {
+    unique_item.quantity += 1;
+  } else {
+    items.push(
+        {id: item.id, name: item.title, quantity: 1, price: item.price});
+  }
+  localStorage.setItem('cart', (JSON.stringify(items)));
+}
+
+// Each item in localstorage is represented as:
+//  {
+//   id: Number
+//   name: String,
+//   quantity: Number,
+//   price: Number
+//  }
+// cart is a list of these items
--- scripts/product_info.js
+++ scripts/product_info.js
@@ -1,39 +1,42 @@
 // This script is used to handle the product information page
-(function () {
-
-  document.querySelector('.btn-submit').addEventListener('click', function (e) {
-    e.preventDefault();
-
-    let quantity = document.querySelector('#quantity').value;
-
-    //temporary action to show the quantity selected;
-    // this should be replaced with adding to local storage list
-    if (quantity < 1) {
-      alert("Please select a quantity greater than 0.");
-    } else {
-      alert(`You have selected ${quantity} items.`);
-    }
-
-  });
+(async function() {
+  let currItem = {};
 
   try {
     let params = new URLSearchParams(window.location.search);
     let product_id = params.get('id');
-    console.log("Product ID:", product_id);
-    fetch(`https://dummyjson.com/products/${product_id}`)
-      .then(response => response.json())
-      .then(data => {
-        // Populate the product information on the page
-        document.querySelector('#product-name').innerHTML = data.title;
-        document.querySelector('#product-description').innerHTML = data.description;
-        document.querySelector('#product-price').innerHTML = `$${data.price}`;
-        document.querySelector('#product-img').src = data.images[0];
-        document.querySelector('#product-img').alt = `An image of: ${data.title}`;
-      })
-      .catch(error => console.error("Error fetching product data:", error));
+    console.log('Product ID:', product_id);
+    await fetch(`https://dummyjson.com/products/${product_id}`)
+        .then(response => response.json())
+        .then(data => {
+          currItem = data;
+          // Populate the product information on the page
+          document.querySelector('#product-name').innerHTML = data.title;
+          document.querySelector('#product-description').innerHTML =
+              data.description;
+          document.querySelector('#product-price').innerHTML = `$${data.price}`;
+          document.querySelector('#product-img').src = data.images[0];
+          document.querySelector('#product-img').alt =
+              `An image of: ${data.title}`;
+        })
+        .catch(error => console.error('Error fetching product data:', error));
 
   } catch (error) {
-    console.error("Error parsing URL parameters:", error);
+    console.error('Error parsing URL parameters:', error);
   }
 
+  document.querySelector('.btn-submit').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let quantity = document.querySelector('#quantity').value;
+
+    if (quantity < 1) {
+      alert('Please select a quantity greater than 0.');
+    } else {
+      alert(`You have selected ${quantity} items.`);
+      for (let i = 0; i < quantity; i++) {
+        add_Cart_Item(currItem);
+      }
+    }
+  });
 })();

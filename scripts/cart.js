d004b9bd9b6b54319c536b2bb96034e319dfd332
Benjamin Glass <glassbg@dukes.jmu.edu>
2025-04-30 11:04:25 -0400

    Click to remove item

--- scripts/cart.js
+++ scripts/cart.js
@@ -12,16 +12,37 @@ function add_Cart_Item(item) {
   if (unique_item != undefined) {
     unique_item.quantity += 1;
   } else {
-    items.push(
-        {id: item.id, name: item.title, quantity: 1, price: item.price});
+    items.push({id: item.id, name: item.title, quantity: 1, price: item.price});
+  }
+  localStorage.setItem('cart', (JSON.stringify(items)));
+  console.log(`Added: ${item.title}`);
+}
+
+  function remove_Cart_Item(item) {
+  console.log(`Attempting to remove: ${item.textContent}`);
+  let items = get_Cart_Items();
+  let unique_item = items.find((i) => i.name == item.name);
+  if (unique_item != undefined && unique_item.quantity > 0) {
+    unique_item.quantity -= 1;
+    console.log(`Removed: ${item.textContent}`);
+    if (unique_item.quantity <= 0) {
+      items.splice(items.indexOf(unique_item), 1);
+    }
+    
   }
   localStorage.setItem('cart', (JSON.stringify(items)));
 }
 
-function calculate_cart_subtotal(){
-    const items = get_Cart_Items();
-    const sum = items.reduce((accumulator, current) => accumulator + (current.price * current.quantity), 0);
-    return sum;
+// function refresh_ul(ul){
+// }
+
+function calculate_cart_subtotal() {
+  const items = get_Cart_Items();
+  const sum = items.reduce(
+      (accumulator, current) =>
+          accumulator + (current.price * current.quantity),
+      0);
+  return sum;
 }
 
 // Each item in localstorage is represented as:
@@ -32,5 +53,3 @@ function calculate_cart_subtotal(){
 //   price: Number
 //  }
 // cart is a list of these items
-
-
--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,8 +1,10 @@
 // Returns a raw JSON array of products
 async function get_all_products(test_mode = false) {
+  console.log('Fetching all products');
   let url = 'https://dummyjson.com/products';
   // If we are in test mode, use the test data instead of the live API
   if (test_mode) {
+    console.log('Locally');
     url = './local_products.json';
   }
 
@@ -11,15 +13,26 @@ async function get_all_products(test_mode = false) {
   return json;
 }
 
-
 async function search_json(e) {
-
   e.preventDefault();
   const q = document.querySelector('.form-control').value.trim();
 
+  // Don't refetch data we already have on search (Possibly add to localstorage to extend usage?)
+  if (q == cached_search.search_term && cached_search.result != undefined) {
+    console.log("Searching for an item (Locally)");
+    document.getElementById('grid-boxes').innerHTML = '';
+    update_grid(cached_search.result);
+    // add ?search=q to the url
+    const url = new URL(window.location.href);
+    url.searchParams.set('search', q);
+    window.history.pushState({}, '', url);
+    return;
+  }
+
   try {
     const res = await fetch(
-      `https://dummyjson.com/products/search?q=${encodeURIComponent(q)}`);
+        `https://dummyjson.com/products/search?q=${encodeURIComponent(q)}`);
+    console.log('Searching for an item (Fetch)')
     const data = await res.json();
 
     // clear out old results
@@ -29,10 +42,15 @@ async function search_json(e) {
     const results = filter_json_for_grid(data);
     update_grid(results);
 
-    //add ?search=q to the url
+    // add ?search=q to the url
     const url = new URL(window.location.href);
     url.searchParams.set('search', q);
     window.history.pushState({}, '', url);
+
+    // Cache the successful search
+    cached_search.search_term = q;
+    cached_search.result = results;
+    console.log(cached_search);
   } catch (err) {
     console.error(err);
   }
@@ -41,7 +59,8 @@ async function search_json(e) {
 // Filter the json array into useful information
 function filter_json_for_grid(json) {
   return json.products.map(
-    element => ({ name: element.title, img_link: element.images[0], id: element.id }));
+      element =>
+          ({name: element.title, img_link: element.images[0], id: element.id}));
 }
 
 function update_grid(products) {
@@ -60,31 +79,31 @@ function update_grid(products) {
     grid_item.querySelector('.product-name').innerHTML = product_name;
     grid_item.querySelector('img').src = image_src;
     grid_item.querySelector('img').alt = `An image of: ${product_name}`;
-    //set link to product page with id
+    // set link to product page with id
     grid_item.querySelector('a').href = `product_info.html?id=${element.id}`;
 
     list.appendChild(grid_item);
   });
 }
 
+let cached_search = {search_term: undefined, result: undefined};
 
-//when the back button is pressed on the browser, reload the page
-window.addEventListener('popstate', function (event) {
+// when the back button is pressed on the browser, reload the page
+window.addEventListener('popstate', function(event) {
   window.location.reload();
 });
 
 document.getElementById('searchSubmitButton')
-  .addEventListener('click', search_json);
+    .addEventListener('click', search_json);
 
-(async function () {
-  //get url parameters from the current page
+(async function() {
+  // get url parameters from the current page
   const urlParams = new URLSearchParams(window.location.search);
   const search = urlParams.get('search');
   if (search !== null) {
     document.querySelector('.form-control').value = search;
     document.getElementById('searchSubmitButton').click();
-  }
-  else {
+  } else {
     let all_products = await get_all_products(true);
     let display_products = filter_json_for_grid(all_products);
     update_grid(display_products);

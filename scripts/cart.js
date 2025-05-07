c85b65fefc7504892099d3ea1f92c6a6a68c275f
krutisbs <krutisbs@dukes.jmu.edu>
2025-05-06 22:36:47 -0400

    Fix undefined event listener when on index.html

--- scripts/cart.js
+++ scripts/cart.js
@@ -12,7 +12,7 @@ function add_Cart_Item(item) {
   if (unique_item != undefined) {
     unique_item.quantity += 1;
   } else {
-    items.push({id: item.id, name: item.title, quantity: 1, price: item.price});
+    items.push({ id: item.id, name: item.title, quantity: 1, price: item.price });
   }
   localStorage.setItem('cart', (JSON.stringify(items)));
   console.log(`Added: ${item.title}`);
@@ -35,16 +35,16 @@ function remove_Cart_Item(item) {
 function calculate_cart_subtotal() {
   const items = get_Cart_Items();
   const sum = items.reduce(
-      (accumulator, current) =>
-          accumulator + (current.price * current.quantity),
-      0);
+    (accumulator, current) =>
+      accumulator + (current.price * current.quantity),
+    0);
   return sum;
 }
 
 function calculate_item_count() {
   const items = get_Cart_Items();
   const sum =
-      items.reduce((accumulator, current) => accumulator + current.quantity, 0);
+    items.reduce((accumulator, current) => accumulator + current.quantity, 0);
   return sum;
 }
 
@@ -68,7 +68,7 @@ function download() {
   const filename = 'shopping_data.json'
   const contentType = 'application/json';
   let a = document.createElement('a');
-  let blob = new Blob([content], {'type': contentType});
+  let blob = new Blob([content], { 'type': contentType });
   a.href = window.URL.createObjectURL(blob);
   a.download = filename;
   a.click();
@@ -87,6 +87,11 @@ async function load_data() {
   }
 }
 
-document.getElementById('export').addEventListener('click', download);
 
-document.getElementById('import').addEventListener('click', load_data);
+//if script is run in the cart page, then run this code
+if (window.location.pathname == '/cart.html') {
+  document.getElementById('export').addEventListener('click', download);
+
+  document.getElementById('import').addEventListener('click', load_data);
+}
+

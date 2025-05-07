8b409c4c48f59afbed045accf521987af630e069
krutisbs <krutisbs@dukes.jmu.edu>
2025-05-07 16:49:17 -0400

    Change Ordering of Add Function and Alert

--- scripts/product_info.js
+++ scripts/product_info.js
@@ -33,8 +33,8 @@
     if (quantity < 1) {
       alert('Please select a quantity greater than 0.');
     } else {
-      alert(`You have selected ${quantity} items.`);
       add_Cart_Item(currItem, quantity);
+      alert(`You have selected ${quantity} items.`);
     }
   });
 })();
